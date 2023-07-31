b14602f14b0f63514338adb8569fc695352f0671
AmirHosseinP2002 <amirhossein.poursaeed1@gmail.com>
2023-07-31 16:01:52 +0330

    add TodoListView and create todos_list.html

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Todo
+
+
+class TodoListView(generic.ListView):
+    # model = Todo -> Todo.objects.all()
+    template_name = 'todos/todos_list.html'
+    context_object_name = 'todos'
+
+    def get_queryset(self):
+        return Todo.objects.filter(author=self.request.user)
