72cc94a161c0c6fe05e286960d0ea8b9196d8575
AmirHosseinP2002 <amirhossein.poursaeed1@gmail.com>
2023-07-31 15:41:24 +0330

    add Todo model and activate in admin page

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_updated = models.DateTimeField(auto_now=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
